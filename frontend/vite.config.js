6b65233d4ff9189c361bb85add32bc24d97704c6
anuj1012 <anujshakya.3m@gmail.com>
2025-03-05 14:06:22 +0530

    unchange

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,16 +7,5 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  build: {
-    chunkSizeWarningLimit: 1000, // Set to 1000KB (1MB)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
-          }
-        }
-      }
-    }
-  }
+ 
 })
